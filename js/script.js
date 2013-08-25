d039ecc88ef10b1b51c5f42da644cb764114512e
Anup Arakkal <anup.anup2001@gmail.com>
2013-08-25 13:32:27 +0300

    Added month picker for date. fixes #9

--- js/script.js
+++ js/script.js
@@ -47,6 +47,19 @@ $(document).ready(function() {
 
     });
 
+    //Configure datePicker
+    $('.date-picker').datepicker( {
+        changeMonth: true,
+        changeYear: true,
+        showButtonPanel: true,
+        dateFormat: 'mmyy',
+        onClose: function(dateText, inst) { 
+            var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+            var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+            $(this).datepicker('setDate', new Date(year, month, 1));
+        }
+    });
+
 
 });
 
