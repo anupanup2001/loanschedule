2f4927016be1701633bc35a6b1021ab8a61e4f7a
arunarakkal162 <arun.arakkal.162@gmail.com>
2013-10-11 11:01:49 +0530

    Fade effect to table. Fade is removed on clicking calculate

---
+++ js/temp.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('#haze').fadeTo(2000,0.2);
+
+});
+
+$(document).ready(function() {
+  $('#btnCalculate').click(function() {
+    $('#haze').fadeTo(1000,1);
+  });
+});
