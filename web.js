b09a4e343e3df59d20a112b41f27a54b5352b9cd
Anup Arakkal <anup.anup2001@gmail.com>
2013-10-14 20:23:01 +0530

    Added 404 catchall link

--- web.js
+++ web.js
@@ -6,6 +6,13 @@ var app = express();
 var nodemailer = require('nodemailer');
 //console.log(process.env.FBMAILP);
 app.use(express.bodyParser());
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use('/images', express.static(__dirname + '/images'));
+app.use(express.favicon(__dirname + '/images/favicon.ico'));
+app.use(app.router);
+
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
     //fs.readFileSync('index.html');
@@ -109,13 +116,14 @@ app.post('/sendEmail', function(request, response) {
     
 });
 
-app.configure(function(){
-    app.use('/js', express.static(__dirname + '/js'));
-    app.use('/css', express.static(__dirname + '/css'));
-    app.use('/fonts', express.static(__dirname + '/fonts'));
-    app.use('/images', express.static(__dirname + '/images'));
-    app.use(express.favicon(__dirname + '/images/favicon.ico'));
+
+app.get('/*', function(request, response) {
+    fs.readFile('error.html', function(err, data) {
+        if (err) {throw err;}
+        response.send(data.toString(), 404);
+    });
 });
+
 var port = process.env.PORT || 8888;
 app.listen(port, function() {
     console.log("Listening on " + port);
